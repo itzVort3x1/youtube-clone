831a48bc6b5c213b4838a261c6d06eb1f42b8215
itzVort3x1 <saikaustubh10@gmail.com>
2023-02-21 11:51:49 +0530

    added swr in searchFeed component

--- client/src/components/SearchFeed.tsx
+++ client/src/components/SearchFeed.tsx
@@ -4,19 +4,27 @@ import VideosComponent from "./Videos";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { useStore } from "@nanostores/react";
 import { isLoggedIn } from "../store/store";
+import useSWR from "swr";
 
 const SearchFeed = () => {
-	const [videos, setVideos] = useState<any[]>([]);
 	const $isLoggedIn = useStore(isLoggedIn);
 
 	const searchTerm = window.location.pathname.split("/").pop();
 
+	const fetcher = () =>
+		fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((res) => res);
+
+	const { data, mutate, error, isLoading } = useSWR(
+		`/searchFeed/${searchTerm}`,
+		fetcher
+	);
+
 	useEffect(() => {
-		fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
-			setVideos(data.items);
-		});
+		mutate();
 	}, [searchTerm]);
 
+	if (error) return <div>Failed to load</div>;
+	if (isLoading) return <div>Loading...</div>;
 	return (
 		<div className="flex lg:flex-row sm:flex-col bg-slate-800 text-white mt-12 p-4">
 			{/* <div className="px-2 border-r-2 border-red-300 fixed overflow-auto">
@@ -37,7 +45,7 @@ const SearchFeed = () => {
 						{searchTerm.replace("%20", " ")}
 					</span>
 				</div>
-				<VideosComponent videos={videos} grids={"grid-cols-4"} />
+				<VideosComponent videos={data?.items} grids={"grid-cols-4"} />
 			</div>
 		</div>
 	);
